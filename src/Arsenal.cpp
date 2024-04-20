c1ddc0e2cd7d70e4172faf13bf65c95d79a948b9
Willow McGinley <127216301+WillowMcG@users.noreply.github.com>
2024-04-20 18:41:54 -0400

    defined getters & sorts

--- src/Arsenal.cpp
+++ src/Arsenal.cpp
@@ -123,6 +123,10 @@ Arsenal::Arsenal() {
     powWeapons.push_back(linearFusions);
     powWeapons.push_back(heavyMachineGuns);
 
+    allWeapons.push_back(priWeapons);
+    allWeapons.push_back(secWeapons);
+    allWeapons.push_back(powWeapons);
+
     cout << "Primary weapon sizes: " << endl;
     for (auto const& i : priWeapons) {
         cout << i.size() << endl;
@@ -183,4 +187,228 @@ void Arsenal::setDPS(bool burst) {
     }
 }
 
+// Used the COP3530 Module 8 Lecture & Discussion Slides
+void Arsenal::swap(vector<Weapon>& v, int a, int b) {
+    Weapon temp = v[a];
+    v[a] = v[b];
+    v[b] = temp;
+}
 
+int Arsenal::partition(vector<Weapon>& vec, int low, int high, bool isSust, bool isFwd) {
+
+    int pivot = (isSust ? vec[low].getSusDPS() : vec[low].getBurstDPS());
+    int up = low, down = high;
+
+    while (up < down) {
+
+        for (int j = up; j < high; j++) {
+
+            if ((isFwd && (isSust ? vec[up].getSusDPS() < pivot : vec[up].getBurstDPS() < pivot)) ||
+                (!isFwd && (isSust ? vec[up].getSusDPS() > pivot : vec[up].getBurstDPS() > pivot))) {
+                break;
+            }
+
+            up++;
+        }
+        for (int j = down; j > low; j--) {
+
+            if ((isFwd && (isSust ? vec[down].getSusDPS() > pivot : vec[down].getBurstDPS() > pivot)) ||
+                (!isFwd && (isSust ? vec[down].getSusDPS() < pivot : vec[down].getBurstDPS() < pivot))) {
+                break;
+            }
+
+            down--;
+        }
+
+        if (up < down) {
+            swap(vec, up, down);
+        }
+    }
+
+    swap(vec, low, down);
+    return down;
+}
+
+void Arsenal::quickSort(vector<Weapon>& vec, int low, int high, bool isSust, bool isFwd) {
+
+    if (low < high) {
+        int pivot = partition(vec, low, high, isSust, isFwd);
+        quickSort(vec, low, pivot - 1, isSust, isFwd);
+        quickSort(vec, pivot + 1, high, isSust, isFwd);
+    }
+}
+
+void Arsenal::insertionSort(vector<Weapon>& vec, int gap, int n, bool isSust, bool isFwd) {
+
+    for (int i = gap; i < n; i++) {
+
+        int key = (isSust ? vec[i].getSusDPS() : vec[i].getBurstDPS());
+        int j = i;
+
+        while (j >= gap && ((isFwd && (isSust ? key > vec[j - gap].getSusDPS() : key > vec[j - gap].getBurstDPS())) ||
+            (!isFwd && (isSust ? key < vec[j - gap].getSusDPS() : key < vec[j - gap].getBurstDPS())))) {
+            swap(vec, j, j - gap);
+            j -= gap;
+        }
+    }
+}
+
+void Arsenal::shellSort(vector<Weapon>& vec, bool isSust, bool isFwd) {
+
+    int n = vec.size();
+    int gap = n / 2;
+
+    while (gap > 0) {
+
+        insertionSort(vec, gap, n, isSust, isFwd);
+
+        if (gap == 2) {
+            gap = 1;
+        }
+
+        else {
+            gap /= 2;
+        }
+    }
+}
+
+Weapon Arsenal::searchItem(vector<vector<vector<Weapon>>> vec, string itemName) {
+
+    Weapon ret;
+
+    for (int i = 0; i < vec.size(); i++) {
+
+        for (int j = 0; j < vec[i].size(); j++) {
+
+            if (vec[i][j].size() == itemName.size()) {
+
+                for (int k = 0; k < itemName.size(); k++) {
+
+                    if (vec[i][j][k].getName() == itemName) {
+                        return vec[i][j][k];
+                    }
+                }
+            }
+        }
+    }
+
+    return ret;
+}
+
+Weapon Arsenal::searchIndex(vector<Weapon> vec, int index) {
+
+	Weapon ret;
+
+    if (index >= 0 && index < vec.size()) {
+        return vec[index];
+    }
+
+    return ret;
+}
+
+vector<Weapon> Arsenal::searchSub(vector<vector<vector<Weapon>>> vec, string itemName) {
+
+    vector<Weapon> ret;
+
+    for (int i = 0; i < itemName.size(); i++) {
+        itemName[i] = tolower(itemName[i]);
+    }
+
+    if (itemName == "autorifles") {
+		return autoRifles;
+	}
+	else if (itemName == "bows") {
+		return bows;
+	}
+	else if (itemName == "fusionrifles") {
+		return fusionRifles;
+	}
+	else if (itemName == "glaives") {
+		return glaives;
+	}
+	else if (itemName == "sec_grenades") {
+		return sec_grenades;
+	}
+	else if (itemName == "handcannons") {
+		return handCannons;
+	}
+	else if (itemName == "pow_grenades") {
+		return pow_grenades;
+	}
+	else if (itemName == "heavymachineguns") {
+		return heavyMachineGuns;
+	}
+	else if (itemName == "linearfusions") {
+		return linearFusions;
+	}
+	else if (itemName == "pulserifles") {
+		return pulseRifles;
+	}
+	else if (itemName == "rockets") {
+		return rockets;
+	}
+	else if (itemName == "scoutrifles") {
+		return scoutRifles;
+	}
+	else if (itemName == "sidearms") {
+		return sidearms;
+	}
+	else if (itemName == "shotguns") {
+		return shotguns;
+	}
+	else if (itemName == "sniperrifles") {
+		return sniperRifles;
+	}
+	else if (itemName == "submachineguns") {
+		return subMachineGuns;
+	}
+	else if (itemName == "swords") {
+		return swords;
+	}
+	else if (itemName == "tracerifles") {
+		return traceRifles;
+	}
+
+    return ret;
+}
+
+vector<vector<Weapon>> Arsenal::searchFam(vector<vector<vector<Weapon>>> vec, string itemName) {
+
+    vector<vector<Weapon>> ret;
+
+    for (int i = 0; i < itemName.size(); i++) {
+        itemName[i] = tolower(itemName[i]);
+    }
+
+    if (itemName == "priweapons") {
+        return priWeapons;
+    }
+	else if (itemName == "secweapons") {
+		return secWeapons;
+	}
+	else if (itemName == "powweapons") {
+		return powWeapons;
+	}
+
+    return ret;
+}
+
+int Arsenal::getIndex(vector<vector<vector<Weapon>>> vec, Weapon item) {
+
+    int foundIndex = -1;
+
+    for (int i = 0; i < vec.size(); i++) {
+
+        for (int j = 0; j < vec[i].size(); j++) {
+
+			for (int k = 0; k < vec[i][j].size(); k++) {
+
+				if (vec[i][j][k] == item) {
+					return foundIndex;
+				}
+			}
+		}
+    }
+
+    return -1;
+}
